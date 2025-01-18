d4a48d550abcc91d111c1f916e4b99b5c608240c
Sergey Shappo <shappoff@gmail.com>
2025-01-18 23:11:11 +0300

    fix default

--- src/app/api/json/route.tsx
+++ src/app/api/json/route.tsx
@@ -2,7 +2,7 @@ import type { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
 import { promises as fs } from "node:fs";
 
-export async function GET(req: NextRequest) {
+export default async function GET(req: NextRequest) {
     const path = "./src/app/api/json/result.json";
     const fileBuffer = await fs.readFile(path);
     const json = JSON.parse(fileBuffer.toString());
