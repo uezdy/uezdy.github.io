c40c915c4d06560f89f01b5056f9ce8eba1c6d61
Sergey Shappo <shappoff@gmail.com>
2025-01-19 22:35:42 +0300

    MainPage

--- src/app/api/json/route.tsx
+++ src/app/api/json/route.tsx
@@ -2,6 +2,8 @@ import { NextResponse } from "next/server";
 import { promises as fs } from "node:fs";
 import * as path from 'path';
 
+export const dynamic = "force-static";
+
 export async function GET(req: any, res: any) {
     const pathJSON = path.join('public/result.json');
     const fileBuffer = await fs.readFile(pathJSON);
