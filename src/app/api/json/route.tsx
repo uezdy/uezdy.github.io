0ac89e9372ca85af7d3812ff7709946bb712e669
Sergey Shappo <shappoff@gmail.com>
2025-01-19 21:37:30 +0300

    fix types

--- src/app/api/json/route.tsx
+++ src/app/api/json/route.tsx
@@ -1,12 +1,10 @@
-import type { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
 import { promises as fs } from "node:fs";
-import {NextApiResponse} from "next";
 import * as path from 'path';
 
-export async function GET(req: NextRequest, res: NextApiResponse<any>) {
+export async function GET(req: any, res: any) {
     const pathJSON = path.join('public/result.json');
     const fileBuffer = await fs.readFile(pathJSON);
     const json = JSON.parse(fileBuffer.toString());
-    return NextResponse.json(json);
+    return NextResponse.json(json) as any;
 }
