a55c2704312d8faa1ba741a9d33383abd8c46cea
Sergey Shappo <shappoff@gmail.com>
2025-01-19 21:29:59 +0300

    fix pathJSON

--- src/app/api/json/route.tsx
+++ src/app/api/json/route.tsx
@@ -2,10 +2,11 @@ import type { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
 import { promises as fs } from "node:fs";
 import {NextApiResponse} from "next";
+import * as path from 'path';
 
 export async function GET(req: NextRequest, res: NextApiResponse<any>) {
-    const path = "./src/app/api/json/result.json";
-    const fileBuffer = await fs.readFile(path);
+    const pathJSON = path.join('public/result.json');
+    const fileBuffer = await fs.readFile(pathJSON);
     const json = JSON.parse(fileBuffer.toString());
     return NextResponse.json(json);
 }
