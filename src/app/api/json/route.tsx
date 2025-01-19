350cfcb321702c29e47f83f08ae53400025f261c
Sergey Shappo <shappoff@gmail.com>
2025-01-19 22:29:58 +0300

    Revert "fix types"
    This reverts commit 588bece955bdf4e41a532a2693b58647c7365adf.

---
+++ src/app/api/json/route.tsx
@@ -0,0 +1,10 @@
+import { NextResponse } from "next/server";
+import { promises as fs } from "node:fs";
+import * as path from 'path';
+
+export async function GET(req: any, res: any) {
+    const pathJSON = path.join('public/result.json');
+    const fileBuffer = await fs.readFile(pathJSON);
+    const json = JSON.parse(fileBuffer.toString());
+    return NextResponse.json(json) as any;
+}
