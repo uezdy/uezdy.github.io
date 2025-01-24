e94a8421491475dd93332ad9d57464a239ef410d
Sergey Shappo <shappoff@gmail.com>
2025-01-24 11:28:01 +0300

    refactoring

--- src/app/sitemap.tsx
+++ src/app/sitemap.tsx
@@ -11,13 +11,13 @@ export default function sitemap(): MetadataRoute.Sitemap {
             priority: 1,
         },
         {
-            url: 'https://uezdy.github.io/',
+            url: 'https://uezdy.github.io/uezdy',
             lastModified: new Date(),
             changeFrequency: 'monthly',
             priority: 0.8,
         },
         {
-            url: 'https://uezdy.github.io/',
+            url: 'https://uezdy.github.io/uezdy',
             lastModified: new Date(),
             changeFrequency: 'weekly',
             priority: 0.5,
