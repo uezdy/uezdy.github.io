a2151d5e143dd262e88e43de8a8fc2e9ed39e151
Sergey Shappo <shappoff@gmail.com>
2025-01-25 00:04:13 +0300

    refactoring

--- src/app/sitemap.tsx
+++ src/app/sitemap.tsx
@@ -1,8 +1,18 @@
-import type { MetadataRoute } from 'next'
+import {messages} from "@/app/services/service.data";
+import {TGMessage} from "@/app/components/types";
 
 export const dynamic = 'force-static';
 
-export default function sitemap(): MetadataRoute.Sitemap {
+export default async function sitemap() {
+    const nestedURLs = messages
+        .filter((msg: TGMessage) => msg.action === 'topic_created')
+        .map((msg: TGMessage) => ({
+            url: `https://uezdy.github.io/uezdy/${msg.id}`,
+            lastModified: new Date(),
+            changeFrequency: 'weekly',
+            priority: 1,
+        }));
+
     return [
         {
             url: 'https://uezdy.github.io/',
@@ -22,5 +32,6 @@ export default function sitemap(): MetadataRoute.Sitemap {
             changeFrequency: 'weekly',
             priority: 0.5,
         },
+        ...nestedURLs
     ]
 }
