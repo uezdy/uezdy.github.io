ffb8fc418b71313db71d324a76cde1b8bb8b1430
Sergey Shappo <shappoff@gmail.com>
2025-01-27 10:59:18 +0300

    fix sitemap

--- src/app/sitemap.tsx
+++ src/app/sitemap.tsx
@@ -1,36 +1,46 @@
-import {messages} from "@/app/services/service.data";
+import {topicsPool} from "@/app/services/service.data";
 import {TGMessage} from "@/app/components/types";
 
 export const dynamic = 'force-static';
 
 export default async function sitemap() {
-    const nestedURLs = messages
-        .filter((msg: TGMessage) => msg.action === 'topic_created')
-        .map((msg: TGMessage) => ({
-            url: `https://uezdy.github.io/uezdy/${msg.id}`,
-            lastModified: new Date(),
-            changeFrequency: 'weekly',
-            priority: 1,
-        }));
+    const nestedURLs: Array<any> = [];
+    const ids: Array<any> = [];
+
+    for (const topicsPoolKey in topicsPool) {
+        const msg: TGMessage = topicsPool[topicsPoolKey];
+        ids.push({
+            topicId: `${msg.id}`,
+            page: `${1}`
+        });
+    }
+
+    ids.forEach(({topicId}: any) => {
+        const pagesArr = topicsPool[topicId].messages;
+        new Array(pagesArr.length)
+            .fill(0)
+            .forEach((v: any, index: number) => {
+                nestedURLs.push({
+                    url: `https://uezdy.github.io/uezdy/${topicId}/${index + 1}`,
+                    lastModified: new Date(),
+                    changeFrequency: 'weekly',
+                    priority: 1,
+                })
+            })
+    });
 
     return [
         {
             url: 'https://uezdy.github.io/',
             lastModified: new Date(),
             changeFrequency: 'weekly',
-            priority: 1,
-        },
-        {
-            url: 'https://uezdy.github.io/uezdy',
-            lastModified: new Date(),
-            changeFrequency: 'weekly',
-            priority: 0.8,
-        },
-        {
-            url: 'https://uezdy.github.io/uezdy',
-            lastModified: new Date(),
-            changeFrequency: 'weekly',
             priority: 0.5,
+        },
+        {
+            url: 'https://uezdy.github.io/uezdy',
+            lastModified: new Date(),
+            changeFrequency: 'weekly',
+            priority: 0.6,
         },
         ...nestedURLs
     ]
