73aeb460f1e7f9d45ad16322027f76d33a6665cb
Sergey Shappo <shappoff@gmail.com>
2025-01-29 00:31:44 +0300

    add prepareExportedData.js

--- prepareExportedData.js
+++ prepareExportedData.js
@@ -1,6 +1,8 @@
 const path = require('path');
 const fs = require('fs');
 
+const perChunk = 2000;
+const targetDir = path.resolve('public/uezdy/src1');
 const pathJSON = path.join('public/uezdy/result.json');
 const file = fs.readFileSync(pathJSON, 'utf8');
 const data = JSON.parse(file);
@@ -46,4 +48,44 @@ data.messages.forEach((message, index) => {
     }
 });
 
+for (const topicId in pool) {
+    let count = 2000;
+    let topicChunk = {};
+    const messagesObject = pool[topicId];
+    for (const messageId in messagesObject) {
+        topicChunk[messageId] = messagesObject[messageId];
+        if (Object.keys(topicChunk).length >= (count - 1)) {
+            const fileName = normalazePageNumb(`${count}`);
+            if (!fs.existsSync(`${targetDir}/${topicId}`)){
+                fs.mkdirSync(`${targetDir}/${topicId}`);
+            }
+            fs.writeFileSync(`${targetDir}/${topicId}/${fileName}.json`, JSON.stringify(topicChunk, null, 4), {encoding: 'utf8', flag: 'w'});
+            topicChunk = {};
+            count += 2000;
+        }
+    }
+}
 
+function normalazePageNumb(page) {
+    let additional = '';
+    switch (page.length) {
+        case 1:
+            additional = '000000';
+            break;
+        case 2:
+            additional = '00000';
+            break;
+        case 3:
+            additional = '0000';
+            break;
+        case 4:
+            additional = '000';
+            break;
+        case 5:
+            additional = '00';
+            break;
+        default:
+            additional = '0';
+    }
+    return `${additional}${page}`;
+}
