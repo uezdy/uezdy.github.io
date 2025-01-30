0577c8a0c106dedf213915517353dfcfe6a0e442
Sergey Shappo <shappoff@gmail.com>
2025-01-30 10:19:09 +0300

    fix sitemap.tsx

--- src/app/sitemap.tsx
+++ src/app/sitemap.tsx
@@ -1,27 +1,19 @@
-import {topicsPool} from "@/app/services/service.data";
+import {topics, topicsPool} from "@/app/services/service.data";
 import {TGMessage} from "@/app/components/types";
 
 export const dynamic = 'force-static';
 
 export default async function sitemap() {
     const nestedURLs: Array<any> = [];
-    const ids: Array<any> = [];
+    const ids: Array<any> = Object.values(topicsPool);
 
-    for (const topicsPoolKey in topicsPool) {
-        const msg: TGMessage = topicsPool[topicsPoolKey];
-        ids.push({
-            topicId: `${msg.id}`,
-            page: `${1}`
-        });
-    }
+    ids.forEach(({id}: any) => {
 
-    ids.forEach(({topicId}: any) => {
-        const pagesArr = topicsPool[topicId].messages;
-        new Array(pagesArr.length)
-            .fill(0)
+        const pagesArr = Object.keys(topics[id]);
+        pagesArr
             .forEach((v: any, index: number) => {
                 nestedURLs.push({
-                    url: `https://uezdy.github.io/uezdy/${topicId}/${index + 1}`,
+                    url: `https://uezdy.github.io/uezdy/${id}/${index + 1}`,
                     lastModified: new Date(),
                     changeFrequency: 'weekly',
                     priority: 1,
