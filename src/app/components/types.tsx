0562f81dd811fe945225a58a12403f89c29baec4
Sergey Shappo <shappoff@gmail.com>
2025-01-30 16:15:31 +0300

    update types

--- src/app/components/types.tsx
+++ src/app/components/types.tsx
@@ -1,27 +1,50 @@
 export interface TGMessage {
-    id:                   number;
-    type?:                string;
-    date?:                Date;
-    date_unixtime?:       string;
-    edited?:              Date;
-    edited_unixtime?:     string;
-    title?:               string;
-    action?:              string;
-    from?:                string;
-    from_id?:             string;
-    reply_to_message_id?: number;
-    text?:                string;
-    text_entities?:       TextEntity[];
-    name?:                string;
-    founded?:             number;
-    members?:             string[];
-    messages:             string[];
+    id: number
+    type: string
+    date: string
+    date_unixtime: string
+    from?: string
+    from_id?: string
+    reply_to_message_id?: number
+    text: any
+    text_entities: TextEntity[]
+    edited?: string
+    edited_unixtime?: string
+    reactions?: Reaction[]
+    actor?: string
+    actor_id?: string
+    action?: string
+    members?: string[]
+    forwarded_from?: string
+    photo?: string
+    width?: number
+    height?: number
+    file?: string
+    file_name?: string
+    thumbnail?: string
+    mime_type?: string
+    message_id?: number
 }
 
 export interface TextEntity {
-    type: string;
-    text: string;
-    href?: string;
+    type: string
+    text: string
+    href?: string
+    language?: string
+    user_id?: number
+}
+
+export interface Reaction {
+    type: string
+    count: number
+    emoji: string
+    recent?: Recent[]
+}
+
+export interface Recent {
+    from?: string
+    from_id: string
+    date: string
 }
 
 export interface SiteMapItem {
