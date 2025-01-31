93a510536fb98ff7132bbb076c832300566c29eb
Sergey Shappo <shappoff@gmail.com>
2025-01-31 14:46:18 +0300

    update menu

--- src/app/components/GroupsMenu.tsx
+++ src/app/components/GroupsMenu.tsx
@@ -1,14 +1,16 @@
 import Link from 'next/link'
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
+import './GroupsMenu.css';
 
 export default function GroupsMenu({groupsList}: any) {
     return <nav>
         <List key={0}>
             {
                 groupsList.map((group: any, index: number) => <ListItem key={index}>
-                    <Link className="topic-list-item" href={`/${group.groupNickName}`}>
-                        {group.title}
+                    <Link className="group-list-item" href={`/${group.groupNickName}`}>
+                        <img className="icon" width="15rem" src={`/${group.groupNickName}/favicon.ico`} alt={group.title}/>
+                        <span>{group.title}</span>
                     </Link>
                 </ListItem>)
             }
