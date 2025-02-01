72aa8055023d139179eb7bf62faea862f225503d
Sergey Shappo <shappoff@gmail.com>
2025-02-02 00:09:25 +0300

    static

--- src/app/components/TopicsList.tsx
+++ src/app/components/TopicsList.tsx
@@ -1,34 +1,17 @@
-'use client'
-import React from "react";
 import Link from 'next/link'
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import CommentIcon from '@mui/icons-material/Comment';
 
 export default function TopicsList({uezd, topicsPool}: any) {
-    const [tops, setTops] = React.useState<any>([]);
-    React.useEffect(() => {
-        if (topicsPool) {
-            const topics: any = [];
 
-            for (const topicsPoolKey in topicsPool) {
-                const msg: any = topicsPool[topicsPoolKey];
-                topics.push({
-                    id: msg.id,
-                    title: msg.title
-                });
-            }
-
-            setTops(topics);
-        }
-    }, [topicsPool]);
     return <nav>
         <List key={0} sx={{
             height: '96vh',
             overflow: 'auto'
         }}>
             {
-                tops.map((top: any) => <ListItem key={top.id}>
+                Object.values(topicsPool).map((top: any) => <ListItem key={top.id}>
                     <CommentIcon className="comment-icon"/>
                     <Link className="topic-list-item" href={`/${uezd}/${top.id}/1`}>
                         {top.title}
