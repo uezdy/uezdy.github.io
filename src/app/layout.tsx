d7ef8d5b57f7fa1b88c3ed7813fa15cfc5d8ed5b
Sergey Shappo <shappoff@gmail.com>
2025-02-02 15:35:16 +0300

    fix viewport error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,20 @@
-import type {Metadata} from "next";
+import type {Metadata, Viewport} from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
     verification: {
         google: "WcZLxrvNHupEwOXBZ_xza8RMaDFrJ_7Nc_Ax_vyo0zw",
         yandex: "9c6b753fbeb916ac"
-    },
-    viewport: {
-        width: 'device-width',
-        initialScale: 1,
-        maximumScale: 1
     }
 };
+export const viewport: Viewport = {
+    width: 'device-width',
+    initialScale: 1,
+    maximumScale: 1,
+    userScalable: false,
+    // Also supported but less commonly used
+    // interactiveWidget: 'resizes-visual',
+}
 
 export default function RootLayout({
   children,
