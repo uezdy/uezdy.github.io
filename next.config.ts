348dc29e9875dfe481ab62bd41fe9dbc909bbfb9
Sergey Shappo <shappoff@gmail.com>
2025-02-10 11:53:39 +0300

    refactor data str

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type {NextConfig} from "next";
 
 const nextConfig: NextConfig = {
     output: 'export',
-    staticPageGenerationTimeout: 300
+    staticPageGenerationTimeout: 400
 };
 
 export default nextConfig;
