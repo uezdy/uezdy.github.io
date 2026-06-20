58857b38d7f152a99795c24cb404922d0a4146e9
Sergey Shappo <shappoff@gmail.com>
2026-06-20 23:09:18 +0300

    add GoogleAnalytics

---
+++ src/components/analytics/GoogleAnalytics.tsx
@@ -0,0 +1,27 @@
+import Script from 'next/script';
+import { getGaMeasurementId } from '@/lib/analytics';
+
+export function GoogleAnalytics() {
+  const measurementId = getGaMeasurementId();
+
+  if (!measurementId) {
+    return null;
+  }
+
+  return (
+    <>
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${measurementId}`}
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${measurementId}');
+        `}
+      </Script>
+    </>
+  );
+}
