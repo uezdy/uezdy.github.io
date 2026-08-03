c854773ef58c382f50845605d59b8884d908cb03
Sergey Shappo <shappoff@gmail.com>
2026-08-03 23:39:08 +0300

    fix(deploy): add basePath environment variable and support in next.config

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,14 @@
 import type { NextConfig } from 'next';
 
+const basePath = process.env.BASE_PATH ?? "";
+
 const nextConfig: NextConfig = {
-  output: 'export',
   trailingSlash: true,
   images: {
     unoptimized: true,
   },
+  basePath: basePath || undefined,
+  assetPrefix: basePath || undefined,
 };
 
 export default nextConfig;
